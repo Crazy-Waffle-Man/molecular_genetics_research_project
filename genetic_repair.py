df984a0290239ee95c328c0ab73bb37efe0d5ea6
Crazy-Waffle-Man <benjamin.shifflet@gmail.com>
2025-04-29 10:44:18 -0400

    4/29/2025 in-class, pt 1

--- genetic_repair.py
+++ genetic_repair.py
@@ -10,14 +10,15 @@ WIDTH = INIT_WIDTH
 HEIGHT = INIT_HEIGHT
 TITLE = "Exploration of Genetic Repair"
 
+#Declare initial variables; we do this before the game loop starts because we don't want to use processing power to define these each frame.
 save_load = save_load_manager.save_load_system("savedata","savedata")
-gui.button()
 buttons = save_load.load_game_data(
         ["buttons.savedata"],
-        [[]]
+        [[gui.button(pgone.Sprite("buttons/play_button.png", 32, 32, 0, 4, 5), (WIDTH//2, HEIGHT//2))]]
     )
 
 all_actors_list = [buttons]
+blip = False
 
 #Prune all unwanted types from the list. Typed arrays are better sometimes.
 to_remove = []
@@ -27,22 +28,31 @@ for button in buttons:
 for item in to_remove:
     buttons.remove(item)
     print(f"{item} ({type(item)}) removed from buttons: not a gui.button")
+#Remove the to_remove list because it is no longer used after this
+del(to_remove)
 
+#Dynamically change the positions of all actors upon the change of screen size
 def update_pos(list):
     """
-    Updates the position of items in [list]
+    Updates the position of items in [list], used when fullscreen
     Args:
         list (list): list of SpriteActors/Actors or their children classes
     """
-    for actor in list:
-        actor.x_ratio = actor.x // INIT_WIDTH
-        actor.y_ratio = actor.y // INIT_HEIGHT
-        actor.x = actor.x_ratio * WIDTH
-        actor.y = actor.y_ratio * HEIGHT
+    for nested_list in list:
+        for actor in nested_list:
+            actor.x_ratio = actor.x // INIT_WIDTH
+            actor.y_ratio = actor.y // INIT_HEIGHT
+            actor.x = actor.x_ratio * WIDTH
+            actor.y = actor.y_ratio * HEIGHT
 
 def draw():
     screen.clear()
-    screen.fill("white")
+    screen.fill("yellow")
+
+    #Automatic draw order. Can be overridden if necessary by adding more draw statements afterward..
+    for nested_list in all_actors_list:
+        for actor in nested_list:
+            actor.draw()
 
 def update():
     pass
@@ -50,30 +60,35 @@ def update():
 def on_mouse_down(pos):
     for button in buttons:
         if button.mouse_collision_bool(pos):
-            pass
+            match button.get_filename():
+                case "play_button_hover.png":
+                    print("Clicking play_button_hover")
 
-blip = False
 def on_mouse_move(pos):
     global blip
     for button in buttons:
         if button.mouse_collision_bool(pos):
-            button.angle = 10
-            button.image = f"{button.image}_hover"
+            button.scale = 1.5
+            if button.image[-10:] != "_hover.png":
+                button.image = f"{button.image}"[0:-4]+"_hover.png"
             if not blip:
                 sounds.blip.play()
+                blip = True
         else:
-            blip = True
-            button.angle = 0
+            blip = False
+            button.scale = 1.0
+            if button.image[-10:] == "_hover.png":
+                button.image = button.image[:-10]+ ".png"
 
 def on_key_down(key):
+    global WIDTH, HEIGHT
     if key == keys.F:  # Press 'F' to toggle fullscreen
         screen.surface = pygame.display.set_mode((WIDTH, HEIGHT), pygame.FULLSCREEN)
         WIDTH, HEIGHT = pygame.display.Info().current_w, pygame.display.Info().current_h  # Fullscreen size
+        update_pos(all_actors_list)
 
     elif key == keys.ESCAPE:  # Press 'ESC' to return to windowed mode
         screen.surface = pygame.display.set_mode((WIDTH, HEIGHT))
         WIDTH, HEIGHT = 500, 500
-    
-    for actor in all_actors_list:
         update_pos(all_actors_list)
 pgzrun.go()
--- gui.py
+++ gui.py
@@ -1,18 +1,5 @@
 from pgone import *
 class button(SpriteActor):
-    def __init__(self):
-        """
-        A SpriteActor with some extra functionality that detects when a tuple of form (x,y) is within\nthe bounds of the image.
-        Args:
-            sprite_instance (Sprite): The sprite instance to use for animation.
-            position (tuple, optional): Initial position of the actor. Defaults to `POS_TOPLEFT`.
-            anchor_point (tuple, optional): Anchor point of the actor. Defaults to `ANCHOR_CENTER`.
-            **kwargs: Additional arguments for the base Actor class.
-        """
-        # Debug prints
-        # print(self.SpriteActor)
-        # print(self.SpriteActor.sprite)
-        # print(self.SpriteActor.sprite.filename)
     def mouse_collision_bool(self, mouse_pos):
         """
         Use in on_mouse_down(pos) or on_mouse_move(pos) as follows:\n
@@ -20,7 +7,7 @@ class button(SpriteActor):
             if self.mouse_collision_bool(pos):
                 #Your code goes here
         """
-        if self.SpriteActor.right >= mouse_pos[0] and self.SpriteActor.left <= mouse_pos[0] and self.SpriteActor.top <= mouse_pos[1] and self.SpriteActor.bottom >= mouse_pos[1]:
+        if self.bottom >= mouse_pos[1] and self.top <= mouse_pos[1] and self.right >= mouse_pos[0] and self.left <= mouse_pos[0]:
             return True
         else:
             return False
